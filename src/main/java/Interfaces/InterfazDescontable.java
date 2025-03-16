6d935a10475f80fa36cd8ee4e5684f60174397a8
Mauricio Vaquero <va0028032023@unab.edu.sv>
2025-03-15 23:02:18 -0600

    Creación de Todas las Interfaces

---
+++ src/main/java/Interfaces/InterfazDescontable.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Interfaces;
+
+/**
+ *
+ * @author mauri
+ */
+public interface InterfazDescontable  <T>{
+    
+     T calcularDescuento(double precio);
+    
+}
+    
+
