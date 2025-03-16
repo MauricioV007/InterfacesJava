26d614ef6e55f5aa8e51cf64f680291c02aeb194
Mauricio Vaquero <va0028032023@unab.edu.sv>
2025-03-15 23:14:27 -0600

    Creación de la clase Animal

---
+++ src/main/java/ClasesImplementación/Animal.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ClasesImplementación;
+import Interfaces.InterfazAlimentacion;
+/**
+ *
+ * @author mauri
+ */
+public class Animal implements InterfazAlimentacion<String>{
+
+    @Override
+    public String comer() {
+        return "El animal está comiendo directamente del suelo.";
+    }
+    
+}
